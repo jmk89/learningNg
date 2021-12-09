1f2559b4cd22fb2a14d67e1818f8d80f22a09173
jmk <jmkolata@gmail.com>
2021-12-08 20:23:00 -0500

    reflecting authentication state in the UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -2,6 +2,7 @@ import { Observable } from 'rxjs';
 import { AuthResponseData, AuthService } from './auth.service';
 import { NgForm } from '@angular/forms';
 import { Component } from "@angular/core";
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-auth',
@@ -12,7 +13,7 @@ export class AuthComponent {
     isLoading = false;
     error:string = null;
 
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService, private router: Router) {}
 
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
@@ -39,6 +40,7 @@ export class AuthComponent {
             response => {
                 console.log(response);
                 this.isLoading = false;
+                this.router.navigate(["/recipes"]);
             }, 
             errorMessage => {
                 console.log(errorMessage);
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
+
 import { DataStorageService } from './../services/data-storage.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorageService: DataStorageService) { }
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthenticated = false;
+  private userSub: Subscription;
+
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.userSub = this.authService.user
+      .subscribe(user => {
+        this.isAuthenticated = !!user; // note this is equal to !user ? false : true; 
+      });
   }
 
   onSaveData() {
@@ -26,6 +36,10 @@ export class HeaderComponent implements OnInit {
 
   toggleDarkTheme(): void {
     document.body.classList.toggle('dark-theme');
+  }
+
+  ngOnDestroy() {
+    this.userSub.unsubscribe();
   }
 
 }
