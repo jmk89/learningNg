4c36031e9a5fec294ec060b52724e64dd9e7aa21
jmk <jmkolata@gmail.com>
2021-12-10 11:25:45 -0500

    splitting up app module into recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { RecipesModule } from './recipes/recipes.module';
+import { RecipeService } from 'src/app/services/recipe.service';
 import { PlaceholderDirective } from './shared/placeholder/placeholder.directive';
 import { AlertComponent } from './shared/alert/alert.component';
 import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
@@ -5,23 +7,19 @@ import { AuthComponent } from './auth/auth.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RecipeResolver } from './recipes/recipe-detail/recipe-resolver.service';
 import { DropdownDirective } from './shared/dropdown.directive';
-import { RecipesComponent } from './recipes/recipes.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ErrorPageComponent } from './error-page/error-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { NoRecipeSelectedComponent } from './recipes/no-recipe-selected/no-recipe-selected.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { ShoppingListService } from './services/shopping-list.service';
 
 @NgModule({
   declarations: [
@@ -29,28 +27,27 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
     HeaderComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
-    RecipeDetailComponent,
-    RecipesComponent,
     DropdownDirective,
     ErrorPageComponent,
     WelcomeComponent,
     NoRecipeSelectedComponent,
-    RecipeEditComponent,
     AuthComponent,
     LoadingSpinnerComponent,
     AlertComponent,
     PlaceholderDirective
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, //should only be imported in app module, just once
     AppRoutingModule,
     ReactiveFormsModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RecipesModule
   ],
-  providers: [RecipeResolver, 
+  providers: [
+    RecipeResolver, 
+    ShoppingListService,
+    RecipeService,
     {
       provide: HTTP_INTERCEPTORS, 
       useClass: AuthInterceptorService, 
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,36 @@
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { NgModule } from "@angular/core";
+import { RecipesComponent } from './recipes.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { AppRoutingModule } from '../app-routing.module';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    declarations: [
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipesComponent,
+        RecipeEditComponent
+    ],
+    exports: [ //put the declarations here, so that any module which imports
+                //the recipe module can use these components
+        RecipeListComponent,
+        RecipeItemComponent,
+        RecipeDetailComponent,
+        RecipesComponent,
+        RecipeEditComponent
+    ],
+    imports: [
+        AppRoutingModule,
+        ReactiveFormsModule,
+        CommonModule //used to get access to ngIf and ngFor
+    ]
+})
+export class RecipesModule {
+
+}
