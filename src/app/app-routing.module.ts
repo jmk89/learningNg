d8c34be1512b92c926961c7c7127a21351c95fa8
jmk <jmkolata@gmail.com>
2021-12-10 15:38:45 -0500

    preloading the lazy-loaded modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,4 +1,4 @@
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { NgModule } from '@angular/core';
 import { ErrorPageComponent } from './error-page/error-page.component';
 
@@ -22,7 +22,7 @@ const appRoutes: Routes = [
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules})
     ],
     exports: [RouterModule]
 })
