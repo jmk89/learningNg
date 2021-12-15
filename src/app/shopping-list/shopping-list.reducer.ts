43302cedc23402bb73043c1186c4a8a68891abb9
jmk <jmkolata@gmail.com>
2021-12-14 22:53:46 -0500

    adding some starting logic to the reducer

--- src/app/shopping-list/shopping-list.reducer.ts
+++ src/app/shopping-list/shopping-list.reducer.ts
@@ -1,3 +1,4 @@
+import { Action } from "@ngrx/store";
 import { Ingredient } from "../shared/ingredient.model";
 
 const initialState = {
@@ -7,6 +8,14 @@ const initialState = {
     ]
 };
 
-export function shoppingListReducer(state = initialState, action) {
-
+export function shoppingListReducer(state = initialState, action: Action) {
+    switch (action.type) {
+        case "ADD_INGREDIENT": //ngrx convention to use all uppercase for action types
+            return {
+                //spread operator to copy the state, because state changes are always immutable
+                ...state,
+                //new array with old array elements
+                ingredients: [...state.ingredients, action]
+            };
+    }
 }
