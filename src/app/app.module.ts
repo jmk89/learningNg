de9b29b879089accff197db976e30110e63a817e
jmk <jmkolata@gmail.com>
2021-12-14 23:06:48 -0500

    setting up ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
 import { AuthModule } from './auth/auth.module';
 import { LoggingService } from './logging.service';
+import { StoreModule } from "@ngrx/store"
+import { shoppingListReducer } from './shopping-list/store/shopping-list.reducer';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { LoggingService } from './logging.service';
     FormsModule,
     HttpClientModule,
     SharedModule, //HeaderComponent is using dropdownDirective, so need to import SharedModule here
-    CoreModule //this is providing services now
+    CoreModule, //this is providing services now
+    StoreModule.forRoot({shoppingList: shoppingListReducer})
   ],
   bootstrap: [AppComponent],
   // providers: [LoggingService]
--- src/app/shopping-list/store/shopping-list.reducer.ts
+++ src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,6 +1,5 @@
-import { Action } from "@ngrx/store";
 import { Ingredient } from "../../shared/ingredient.model";
-import { ADD_INGREDIENT } from "./shopping-list.actions";
+import * as ShoppingListActions from "./shopping-list.actions";
 
 const initialState = {
     ingredients: [
@@ -9,14 +8,14 @@ const initialState = {
     ]
 };
 
-export function shoppingListReducer(state = initialState, action: Action) {
+export function shoppingListReducer(state = initialState, action: ShoppingListActions.AddIngredient) {
     switch (action.type) {
-        case ADD_INGREDIENT: //ngrx convention to use all uppercase for action types
+        case ShoppingListActions.ADD_INGREDIENT: //ngrx convention to use all uppercase for action types
             return {
                 //spread operator to copy the state, because state changes are always immutable
                 ...state,
                 //new array with old array elements
-                ingredients: [...state.ingredients, action]
+                ingredients: [...state.ingredients, action.payload]
             };
     }
 }
