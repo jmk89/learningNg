7f9a029244dc03f7bcc9d1e8873ca02c59cb0c23
jmk <jmkolata@gmail.com>
2021-12-18 10:47:27 -0500

    dispatching auth actions

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -5,6 +5,9 @@ import { BehaviorSubject, throwError } from 'rxjs';
 import { User } from './user.model';
 import { Router } from '@angular/router';
 import { environment } from '../../environments/environment';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 //defined at https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
 //not a requirement of Angular, but makes life easier to have it defined in an interface
@@ -26,7 +29,11 @@ export class AuthService {
     user = new BehaviorSubject<User>(null);
     private tokenExpirationTimer: any;
 
-    constructor(private http: HttpClient, private router: Router) { }
+    constructor(
+        private http: HttpClient, 
+        private router: Router, 
+        private store: Store<fromApp.AppState>
+    ) { }
 
     //check out https://firebase.google.com/docs/reference/rest/auth#section-create-email-password
     //to see why the post request is constructed this way. The inline javascript object
@@ -87,14 +94,22 @@ export class AuthService {
 
         const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
         if (loadedUser.token) {
-            this.user.next(loadedUser);
+            //this.user.next(loadedUser);
+            //removing the usage of behaviorSubject in favor of dispatching an ngrx action
+            this.store.dispatch(new AuthActions.Login({
+                email: loadedUser.email,
+                userID: loadedUser.id,
+                token: loadedUser.token,
+                expirationDate: new Date(userData._tokenExpirationDate)
+            }))
             const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime();
             this.autoLogout(expirationDuration);
         }
     }
 
     logout() {
-        this.user.next(null);
+        //this.user.next(null);
+        this.store.dispatch(new AuthActions.Logout());
         localStorage.removeItem('userData');
         this.router.navigate(['/auth']);
         if (this.tokenExpirationTimer) {
@@ -112,7 +127,15 @@ export class AuthService {
     private handleAuthentication(email: string, userId: string, token: string, expiresIn: number) {
         const expirationDate = new Date(new Date().getTime() + expiresIn * 1000);
         const user = new User(email, userId, token, expirationDate);
-        this.user.next(user);
+        //this.user.next(user);
+        this.store.dispatch(
+            new AuthActions.Login({
+                email: email,
+                userID: userId,
+                token: token,
+                expirationDate: expirationDate
+            })
+        );
         this.autoLogout(expiresIn * 1000);
         localStorage.setItem('userData', JSON.stringify(user));
     }
